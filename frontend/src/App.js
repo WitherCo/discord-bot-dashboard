39718e26fe38758d4e97e3745a53a982a89d3c8d
Christopher Thomas <mcbestieslife24@gmail.com>
2025-04-30 11:25:47 -0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Discord Bot Dashboard</h1>
+        <a href="http://localhost:5000/auth/discord">Login with Discord</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
